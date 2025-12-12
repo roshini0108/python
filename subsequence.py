02d89b793ee8749f8efdb573313891ce41acdd23
gnaneswari <gnaneswarivana@gmail.com>
2025-12-12 11:41:33 +0530

    longestsubsequence

--- subsequence.py
+++ subsequence.py
@@ -1,4 +1,4 @@
-def PrintSbsq(s,i,res):
+"""def PrintSbsq(s,i,res):
     if i==len(s):
         print(res)
         return
@@ -7,4 +7,25 @@ def PrintSbsq(s,i,res):
     # exclude the character at index i
     PrintSbsq(s,i+1,res)
 print("subsequence")
-PrintSbsq("suji",0,"")
+PrintSbsq("suji",0,"")"""
+# longest common subsequence
+class Python: 
+    def __init__(self):
+        self.res=''
+
+    def printn(self):
+        print(self.res)
+
+    def lcs(self, s1, s2, i, j):
+        if i == len(s1) or j == len(s2):
+            return ''
+        if s1[i] == s2[j]:
+            return s1[i] + self.lcs(s1, s2, i+1, j+1)
+        else:
+            x = self.lcs(s1, s2, i+1, j)
+            y = self.lcs(s1, s2, i, j+1)
+            return x if len(x) > len(y) else y
+
+p = Python()  
+print("Longest Common Subsequence:")
+print(p.lcs("kavya", "kavitha", 0, 0))
